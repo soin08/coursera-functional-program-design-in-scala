d91425dca93ce45e56190ffbd79abbef56bf5469
Xuebing Li <xuebinglee@gmail.com>
2017-03-14 23:36:26 +1100

    implement suggested properties

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -23,4 +23,40 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  // If you insert any two elements into an empty heap, finding the
+  // minimum of the resulting heap should get the smallest of the two
+  // elements back.
+  property("findMin: heap of two elements") = forAll { (e1: A, e2: A) =>
+    val h = insert(e2, insert(e1, empty))
+    val smaller = if (e1 < e2) e1 else e2
+    findMin(h) == smaller
+  }
+
+  // Finding a minimum of the melding of any two heaps should return
+  // a minimum of one or the other.
+  property("findMin: two heaps") = forAll { (h1: H, h2: H) =>
+    (!isEmpty(h1) && !isEmpty(h2)) ==> {
+      val h = meld(h1, h2)
+      findMin(h) == findMin(h1) || findMin(h) == findMin(h2)
+    }
+  }
+
+  // If you insert an element into an empty heap, then delete the
+  // minimum, the resulting heap should be empty.
+  property("deleteMin: heap of one element") = forAll { (e: A) =>
+    val h = insert(e, empty)
+    isEmpty(deleteMin(h))
+  }
+
+  // Given any heap, you should get a sorted sequence of elements
+  // when continually finding and deleting min.
+  property("deleteMin: continually") = forAll { (h: H) =>
+    def heapToList(h: H): List[A] = {
+      if (isEmpty(h)) List()
+      else findMin(h) :: heapToList(deleteMin(h))
+    }
+    val l = heapToList(h)
+    l == l.sorted
+  }
+
 }
