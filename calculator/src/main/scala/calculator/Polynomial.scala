de0d5c757b7d03163ace3ce0657cae81510c9e88
Xuebing Li <xuebinglee@gmail.com>
2017-03-24 15:41:44 +1100

    implement root solver for 2nd degree polynomial

--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,20 @@
 package calculator
 
+import Math.sqrt
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] = {
-    ???
+      c: Signal[Double]): Signal[Double] = Signal {
+    b() * b() - 4 * a() * c()
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = Signal {
+    if (delta() < 0) Set()
+    else {
+      val solution1 = (-b() + sqrt(delta())) / 2 * a()
+      val solution2 = (-b() - sqrt(delta())) / 2 * a()
+      Set(solution1, solution2)
+    }
   }
 }
